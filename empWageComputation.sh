9a96d57e0b4661df6a3df8c8d6eeeeb29f98edee
Rajat504 <rajatravindar504@gmail.com>
2020-02-12 18:22:43 +0530

    [Rajat]add.Fulldaywages method

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE PROGRAM"
+#UserCase2
+FullDayWages()
+{
+Wages_per_hour=20
+FullDay_Working=8
+TotalWagesPerDay=$(($Wages_per_hour*$FullDay_Working))
+echo "TotalWagesPerDay=$TotalWagesPerDay"
+}
+FullDayWages
+
