9a3b241f497497fe053fc5615edbd5632b6ba247
Rajat504 <rajatravindar504@gmail.com>
2020-02-12 17:42:24 +0530

    [Rajat]ADD.added a check Attendance function Program

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,13 @@
-#!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE PROGRAM"
+#user_case 1
+checkAttendance()
+{
+Attendance=$(($Random%2))
+if
+(($Attendance<1))
+then
+echo "Employee is Present"
+else
+echo "Employee is Absent"
+fi
+checkAttendance
+
