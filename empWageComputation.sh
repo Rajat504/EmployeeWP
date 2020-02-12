2980975deeb3cc1efdc79161dcee5694947e9371
Rajat504 <rajatravindar504@gmail.com>
2020-02-12 19:28:09 +0530

    [Rajat]add.added Halfmethod

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE PROGRAM"
+#UserCase 3
+HalfDayWages(){
+Wages_Per_Hour=20
+Part_Time_Hour=4
+TotalWagesPerDay=$(($Wages_Per_Hour*$Part_Time_Hour))
+echo "Total_Wages_Per_Day=$TotalWagesPerDay"
+}
+HalfDayWages
+
