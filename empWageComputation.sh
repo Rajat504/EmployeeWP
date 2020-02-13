66f964f4014b23aa9047cbdba774b5c275d4f52b
Rajat504 <rajatravindar504@gmail.com>
2020-02-13 11:56:52 +0530

    [Rajat]add.added switch case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE PROGRAM"
+#UserCase 4
+echo "1. calculate FullDayWages"
+echo "2. calculate HalfDayWages"
+read cases
+case $cases in
+	1)
+		FullDayWages
+		;;
+	2)
+		HalfDayWages
+		;;
+	*)
+		echo "Kindly enter a valid option"
+		;;
+esac
