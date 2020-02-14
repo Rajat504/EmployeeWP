1b66e8156516d33f80544bb1f5597a10851d8dc3
Rajat504 <rajatravindar504@gmail.com>
2020-02-14 11:29:57 +0530

    [Rajat]add.added working hour of an employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,39 @@
-#!/bin/sh
-echo "WELCOME TO EMPLOYEE WAGE PROGRAM"
+#UC8
+checkAttendance(){
+	Attendance=$(($RANDOM%2))
+	if(($Attendance<1))
+	then
+		return 1
+	else
+		return 0
+	fi
+}
+WorkingHour()
+{
+count=0
+        for((i=0; i<20; i++))
+	do
+		checkAttendance
+		res=$?
+		if(($res==0))
+			then
+			count=$(($count+1))
+		fi
+	done
+	echo "Total Working Hour Of an Employe"
+	echo -e "1.Full Time employee \n2.Part Time Employee"
+	echo "Enter a option"
+	read  option
+		if(($option==1))
+			then
+			TotalWorkHour=$((8*$count))
+		elif(($option==2))
+			then
+			TotalWorkHour=$((4*$count))
+		else
+			echo "Enter a Valid option."
+		fi
+		return $TotalWorkHour
+}
+WorkingHour
+echo "total working hour of an employee is $?"
